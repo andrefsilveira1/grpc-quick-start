d66d12d11fc4ac3fc6d4c65e75f48053086f6adb
André Freitas <freitasandre38@gmail.com>
2024-06-02 18:11:43 -0300

    feat: add haversine package, and create stream connection

--- service/registers.go
+++ service/registers.go
@@ -3,6 +3,10 @@ package service
 import (
 	"andrefsilveira/grpc-quick-start/internal/pb"
 	"io"
+	"strconv"
+
+	"github.com/andrefsilveira1/go-haversine"
+	"github.com/google/uuid"
 )
 
 type RegisterService struct {
@@ -13,8 +17,14 @@ func NewRegisterService() *RegisterService {
 	return &RegisterService{}
 }
 
+var latitude = -5.8623992555733695
+var longitude = -35.19111877919574
+
 func (c *RegisterService) CalculateDistance(stream pb.RegisterService_CreateRegisterStreamServer) error {
 	registers := &pb.Registers{}
+	var checkpoint = 0.0
+	var closer = false
+
 	for {
 		register, err := stream.Recv()
 		if err == io.EOF {
@@ -23,7 +33,28 @@ func (c *RegisterService) CalculateDistance(stream pb.RegisterService_CreateRegi
 		if err != nil {
 			return err
 		}
+		lat, err := strconv.ParseFloat(register.Latitude, 64)
+		lon, err := strconv.ParseFloat(register.Longidute, 64)
+		result := haversine.Calculate(latitude, longitude, lat, lon)
+		value := strconv.FormatFloat(result, 'f', -1, 64)
+		if err != nil {
+			return err
+		}
 
-		// distanceReult, err := haversine.distance(register.Latitude, register.Longidute)
+		if result < checkpoint {
+			checkpoint = result
+			closer = true
+		} else {
+			closer = false
+		}
+
+		registers.Regiters = append(registers.Regiters, &pb.Register{
+			Id:        uuid.New().String(),
+			Latitude:  register.Latitude,
+			Longitude: register.Longidute,
+			Distance:  value,
+			Closer:    closer,
+		})
+
 	}
 }
